ad56ce0fbfb55d92ba6f37c0f3524bb01d7ff332
Tomas Avella <tomasavella007@gmail.com>
2016-12-15 16:27:21 +0100

    Practica 5.6

--- app/src/main/java/hilos/example/net/hilos/MainActivity.java
+++ app/src/main/java/hilos/example/net/hilos/MainActivity.java
@@ -1,6 +1,7 @@
 package hilos.example.net.hilos;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
@@ -79,7 +80,14 @@ public class MainActivity  extends AppCompatActivity {
             progreso = new ProgressDialog(MainActivity.this);
             progreso.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
             progreso.setMessage("Calculando...");
-            progreso.setCancelable(false);
+            //Practica 5.6
+            progreso.setCancelable(true);
+            progreso.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                @Override
+                public void onCancel(DialogInterface dialog) {
+                    MiTarea.this.cancel(true);
+                }
+            });
             progreso.setMax(100);
             progreso.setProgress(0);
             progreso.show();
@@ -88,7 +96,7 @@ public class MainActivity  extends AppCompatActivity {
         protected Integer doInBackground(Integer... n) {
             Integer progreso = new Integer(0);
             int res = 1;
-            for (int i = 1; i <= n[0]; i++) {
+            for (int i = 1; i <= n[0] && !isCancelled(); i++) {
                 res *= i;
                 SystemClock.sleep(1000);
                 progreso = (i * 100) / n[0];
@@ -105,6 +113,12 @@ public class MainActivity  extends AppCompatActivity {
             progreso.dismiss();
             salida.append(res + "\n");
         }
+        //Practica 5.6
+        @Override
+        protected void onCancelled() {
+            salida.append("cancelado\n");
+        }
+
     }
 
 }
